7e25d65f849d074a0247084e5ce1f2b3a4a4dd0c
Facundo Mainere <fmmainere@gmail.com>
2014-04-14 16:19:38 -0300

    Agregadas todas las preguntas al json

--- quiz.js
+++ quiz.js
@@ -44,6 +44,141 @@
         "c":"Delegar",
         "d":"Dirigir"
         }
+		},
+  {
+			"question":"Usted cuenta con un colaborador muy eficiente en tareas administrativas. Su trabajo es de toda confianza y seguridad. Se están discutiendo algunos pequeños cambios en la forma de llevar la administración.¿Qué cree que haría usted en un caso así?",
+			"option": {
+					"a":"Pide opiniones al colaborador y decide los cambios.",
+					"b":"Decide cambios y los comunica al colaborador.",
+					"c":"Hace intervenir al colaborador en las discusiones y delega en él la forma de implementar lo decidido.",
+          "d":"Explica al colaborador las razones de los cambios que fueron decididos."
+			},
+			"ans":{
+        "d":"Persuadir",
+        "a":"Participar",
+        "c":"Delegar",
+        "b":"Dirigir"
+        }
+		},
+  {
+			"question":"Uno de sus colaboradores ha venido obteniendo logros en una tarea que se le asignó recientemente. Usted ha estado controlando y contribuyendo con indicaciones precisas a estos logros. El colaborador siente seguridad para realizar la tarea.¿Cuál cree que sería su manera natural de actuar en un caso así?",
+			"option": {
+					"a":"Deja que el colaborador se desempeñe con total autonomía en esta tarea.",
+					"b":"Pide al colaborador sugerencias de cómo mejorar la realización de la tarea.",
+					"c":"Se reúne con el colaborador y conversan sobre la tarea analizando los riesgos, beneficios y efectos de la tarea, acordando con él cómo continuarán la tarea en el futuro.",
+          "d":"Dados los buenos resultados continuará con el mismo estilo de supervisión de este colaborador."
+			},
+			"ans":{
+        "c":"Persuadir",
+        "b":"Participar",
+        "a":"Delegar",
+        "d":"Dirigir"
+        }
+		},
+  {
+			"question":"Usted cuenta con un colaborador que se ha venido desempeñando satisfactoriamente y de manera bastante autónoma en sus tareas. La restricción de recursos obliga a cambiar algunos de los métodos que está utilizando. ¿Qué cree que haría usted en este caso?",
+			"option": {
+					"a":"Toma las decisiones necesarias y se sienta con su colaborador para explicárselas y darle su fundamentación.",
+					"b":"Informa al colaborador de la necesidad de hacer cambios y delega en él la tarea.",
+					"c":"Decide los cambios y se los comunica al colaborador.",
+          "d":"Deciden juntos los cambios a realizar habiendo establecido los objetivos."
+			},
+			"ans":{
+        "c":"Dirigir",
+        "a":"Persuadir",
+        "d":"Participar",
+        "b":"Delegar"
+        }
+		},
+  {
+			"question":"Recientemente se han incorporado nuevas actividades y las tareas de sus colaboradores se verán modificadas en consecuencia. El grupo es trabajador, siempre ha demostrado voluntad de cooperar. Ellos no están muy informados de lo que estos cambios significan. ¿Cuál sería su actitud?",
+			"option": {
+					"a":"Informa de la necesidad de efectuar cambios y deja que en grupo los implemente.",
+					"b":"Reúne y explica las razones por las que deberán hacerse los cambios y fija los objetivos. Durante la reunión entrega toda la información necesaria para que se comprenda la situación.",
+					"c":"Reúne al grupo y pide las opiniones de los miembros respecto a los cambios a realizar. Decide los cambios y los comunica. Supervisa cuidadosamente.",
+          "d":"Cita al grupo y menciona en detalle los pasos a seguir."
+			},
+			"ans":{
+        "d":"Dirigir",
+        "b":"Persuadir",
+        "c":"Participar",
+        "a":"Delegar"
+        }
+		},
+  {
+			"question":"Usted deberá implementar algunos cambios de procedimiento menores resultantes de un proceso de reingeniería que afecta a sus clientes internos. El grupo conoce la situación, es solvente y autónomo y ha demostrado flexibilidad en otras situaciones semejantes. ¿Qué cree que haría usted?",
+			"option": {
+					"a":"Da la información al grupo y delega la implementación.",
+					"b":"Toma decisiones y en una reunión las comunica fundamentándolas.",
+					"c":"Toma decisiones y las comunica individualmente supervisando cuidadosamente su cumplimiento.",
+          "d":"Reúne al grupo, plantea el problema y pide sugerencias para luego tomar la decisión."
+			},
+			"ans":{
+        "c":"Dirigir",
+        "b":"Persuadir",
+        "d":"Participar",
+        "a":"Delegar"
+        }
+		},
+  {
+			"question":"El grupo que usted conduce ha manejado con autonomía y solvencia sus responsabilidades, pero ahora usted ha notado que están encontrando algunas dificultades que les plantean las últimas actividades propuestas por la mesa directiva. Tiene que hacer un estudio para una importante redistribución de tareas y usted percibe que están algo inquietos.¿Qué cree usted que haría en una situación así?",
+			"option": {
+					"a":"Toma el estudio a su cargo.",
+					"b":"Define con precisión los objetivos y deja que el grupo trabaje autónomamente como siempre.",
+					"c":"Se reúne con el grupo y dirige la discusión en búsqueda del mejor planteo del estudio, estimulando el aporte de ideas.",
+          "d":"Se reúne con el grupo y les explica la importancia de resolver este estudio correctamente. Supervisa con todo cuidado."
+			},
+			"ans":{
+        "a":"Dirigir",
+        "d":"Persuadir",
+        "c":"Participar",
+        "b":"Delegar"
+        }
+		},
+  {
+			"question":"La situación anterior se ha agravado notablemente, se acerca el plazo de la redistribución de tareas y el grupo está desorientado. La gerencia se ha enterado de lo que sucede y lo está presionando por resultados.¿Qué haría en este caso?",
+			"option": {
+					"a":"Se reúne con el grupo y les explica la importancia de resolver este estudio correctamente. Supervisa con todo cuidado.",
+					"b":"Define con precisión los objetivos y deja que el grupo trabaje autónomamente como siempre.",
+					"c":"Se reúne con el grupo y dirige la discusión en búsqueda del mejor planteo del estudio estimulando el aporte de ideas.",
+          "d":"Toma la conducción del estudio a su cargo."
+			},
+			"ans":{
+        "d":"Dirigir",
+        "a":"Persuadir",
+        "c":"Participar",
+        "b":"Delegar"
+        }
+		},
+  {
+			"question":"Al incorporarse las nuevas disposiciones hay un sector que casi no se ha visto modificado. El grupo es eficiente y ha venido respondiendo con seguridad a los pocos cambios que se requirieron. ¿Qué cree usted que haría?",
+			"option": {
+					"a":"Informa que ante la situación de reorganizaciones, será necesario conducir al grupo con instrucciones más precisas y controlar cuidadosamente el desempeño.",
+					"b":"No hace nada. Sigue su conducta habitual.",
+					"c":"Reúne al grupo y fundamenta la necesidad de aumentar el control sobre las tareas.",
+          "d":"Reúne al grupo para discutir y recibir sugerencias de cómo actuar en la presente situación."
+			},
+			"ans":{
+        "a":"Dirigir",
+        "c":"Persuadir",
+        "d":"Participar",
+        "b":"Delegar"
+        }
+		},
+  {
+			"question":"El grupo ha venido reduciendo su desempeño como consecuencia de la disminución de recursos disponibles. Se había caracterizado por ser un grupo dinámico y con frecuencia hacía aportes y sugerencias. La situación actual no es crítica, ni extrema. ¿Qué cree usted que haría?",
+			"option": {
+					"a":"Reúne al grupo y pide sugerencias.",
+					"b":"Reúne al grupo y describe la situación y lo que se espera que suceda en el futuro próximo. Repasa los objetivos y aumenta cuidadosamente el control sobre las tareas.",
+					"c":"No hace nada. El grupo es bueno y reaccionará.",
+          "d":" Incrementa el control sobre el cumplimiento de las tareas, redefine los objetivos y plazos."
+			},
+			"ans":{
+        "d":"Dirigir",
+        "b":"Persuadir",
+        "a":"Participar",
+        "c":"Delegar"
+        }
 		}
 	]
 }
